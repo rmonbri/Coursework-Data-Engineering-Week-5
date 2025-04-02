4473a4c2a366dddf78dba27f154479a758d5c928
Donnell <trainee.donnell.burgess@sigmalabs.co.uk>
2025-04-02 19:14:22 +0100

    Used lambda_multiprocessing for get requests. Working ETL pipeline

--- short-term-pipeline/pipeline.py
+++ short-term-pipeline/pipeline.py
@@ -1,5 +1,6 @@
+# pylint: disable= broad-exception-caught
 """Script to merge extract, transform and load scripts into a single pipeline"""
-
+import logging
 from extract import get_plant_data_multiprocessing, save_to_csv
 from transform import read_data, save_clean_data_to_csv, read_csv_data, clean_data
 from load import get_measurements_from_df, ingress_measurements_to_db, get_measurements_from_csv
@@ -18,23 +19,23 @@ def local_pipeline():
 
 
 def handler(event, context):
-    """Lambda Handler to add measurements"""
-    print(f'Event: {event}')
-    print(f'Context: {context}')
+    """Lambda Handler to add measurements to db"""
+    logging.info('Event: %s', event)
+    logging.info('Context: %s', context)
     try:
         load_dotenv()
-        print("Getting plant data from API")
+        logging.info("Getting plant data from API")
         plant_data = get_plant_data_multiprocessing()
-        print("Plant data extracted")
-        print("Cleaning plant data")
+        logging.info("Plant data extracted")
+        logging.info("Cleaning plant data")
         raw_data = read_data(plant_data)
-        print("Created DataFrame")
+        logging.info("Created DataFrame")
         cleaned_data = clean_data(raw_data)
-        print("Data cleaned")
-        print("Inserting clean data to database")
+        logging.info("Data cleaned")
+        logging.info("Inserting clean data to database")
         plant_measurements = get_measurements_from_df(cleaned_data)
         ingress_measurements_to_db(plant_measurements)
-        print("Data inserted successfully")
+        logging.info("Data inserted successfully")
 
         return {
             'statusCode': 200,
@@ -49,5 +50,4 @@ def handler(event, context):
 
 
 if __name__ == "__main__":
-
     local_pipeline()
